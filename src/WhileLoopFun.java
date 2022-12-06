820593122b8690ed3c700bdf4f489fda0d6d8b63
shahawlhs <shaha@wlhs.wlwv.k12.or.us>
2022-12-06 14:42:55 -0800

    Created while loops

--- src/WhileLoopFun.java
+++ src/WhileLoopFun.java
@@ -26,19 +26,43 @@ public class WhileLoopFun {
     public static void main(String[] args) {
         //count to 10
 
+        /*int counter = 1;
+        while(counter <= 10){
+            System.out.print(counter + " ");
+            counter++;
+        }*/
 
         //what does this code loop do? Do this without running it
-        int count = 2;
+        /*int count = 2;
         while(count <= 100){
             System.out.print(count + " ");
             count *= 2;
+        }*/
+
+        System.out.println();
+        //write a loop that counts by 5, starting at 0, until you get to 100
+        int count1 = 0;
+        while(count1 <= 100){
+            System.out.print(count1 + " ");
+            count1 += 5;
         }
         System.out.println();
-        //write a loop that counts by 5 until you get to 100
-
         //write a loop that counts down from 10 to 1 then prints "Blast off!"
-
+        int count2 = 10;
+        while(count2 >= 1){
+            System.out.println(count2);
+            count2--;
+        }
+        System.out.println("Blast off!");
+        System.out.println();
         //loop mistakes and while vs do while loop
+        int newCount = 5;
+        while(newCount >= 10){
+            System.out.println("This statement has executed.");
+        }
+        do {
+            System.out.println("This will print at least once.");
+        }while(newCount >= 10);
         //make an infinite loop happen
 
 
